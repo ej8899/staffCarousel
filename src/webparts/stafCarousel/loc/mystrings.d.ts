98263abbab011cc27cfae3e60fe71950ee8ee889
ej8899 <ej8899@gmail.com>
2025-05-30 12:27:08 -0400

    ADD: initial build files

---
+++ src/webparts/stafCarousel/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IStafCarouselWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'StafCarouselWebPartStrings' {
+  const strings: IStafCarouselWebPartStrings;
+  export = strings;
+}
